3c9dc2e53090c377c27a0f113f2129432ddf5857
HumzaSyed86 <55002289+HumzaSyed86@users.noreply.github.com>
2021-02-15 19:13:12 -0600

    Add files via upload

--- Project2.cpp
+++ Project2.cpp
@@ -52,23 +52,29 @@ double** multiplyMatricesPtr(
         const uint32_t b_cols) {
 
 //malloc
-double** c = (double**)malloc(sizeof(double));
+double** c = (double**)malloc(sizeof(double*)*a_rows); //a_rows is the memory elements for different row amounts
 
-// code seems the same you can change and do stuff.
+    for(int i = 0; i<a_rows;i++){
+        c[i]= (double*)malloc(sizeof(double)*b_cols); //b_cols is the memory allocations for b_cols
+    }
+
+    for(int a = 0;a<a_rows;a++){
+        for(int b =0;b<b_cols;b++){
+            c[a][b] = 0; // clears c of its garbage
+        }
+    }
+
     for(int i = 0;i<a_rows;i++){ // 0-M, # of rows A  I
         for(int j = 0;j<b_cols;j++){ // 0-K, # of in col bB, J
             for(int k = 0;k<a_cols;k++){ // 0-N, # of col a    , K
 
-
-                **c = **a * **b;
-                // c is M*K a is M*N b is N*K
+                c[i][j] += a[i][k] * b[k][j]; // 2d array using previous test
 
             }
         }
     }
 
     return c;
-    //return NULL;
 }
 
 // https://en.wikipedia.org/wiki/Transpose
@@ -77,6 +83,8 @@ double** transposeMatrix(
         const uint32_t a_rows,
         const uint32_t a_cols) {
 //malloc
+        // need to switch a_ros and a_cols in the info
 
+        //b[a_cols][a_rows] = a[a_rows][a_cols]; // 2 loops
     //return NULL;
 }
